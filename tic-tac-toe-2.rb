36ecf6ed1e42772ff70d5fcc09f30e0e485555e8
asuna-cross <littlecrystaldragon@gmail.com>
2023-03-08 20:39:30 +0000

    Base pseudocode for own attempt

---
+++ tic-tac-toe-2.rb
@@ -0,0 +1,11 @@
+# Array - Input
+
+# Def Input
+# Check Input
+# Split and Add to Array Counter
+# Check Array Counter
+# Output Location
+# Display
+
+# Need to figure out turn tracking
+# Display it all
