11a0b5b98bff5009b341f4c947c41734a2f9c316
asuna-cross <littlecrystaldragon@gmail.com>
2023-03-24 23:22:14 +0000

    Tic Tac Toe working

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,123 +1,71 @@
-puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+#!/usr/bin/env ruby
 
-WIN_COMBINATIONS = [ 
-    [0,1,2], # top_row 
-    [3,4,5], # middle_row 
-    [6,7,8], # bottom_row 
-    [0,3,6], # left_column 
-    [1,4,7], # center_column 
-    [2,5,8], # right_column 
-    [0,4,8], # left_diagonal 
-    [6,4,2] # right_diagonal 
-    ]
+PLAYER_ONE = 0
+PLAYER_TWO = 1
 
-#board displaying the proper board[index] in its array format
-def display_board(board)
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+class Game
 
-def input_to_index(user_input)
-user_input.to_i - 1
-end
-
-def move(board, index, player)
-    board[index] = player
-end
-
-def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false 
-   else
-      return true
-   end
-end
-
-def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index)
-      return true
-   end
-end
-
-def turn_count(board)
-   counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
+  def make_move(board, row, col, turn)
+    print (turn == PLAYER_ONE) ? "First player's turn: " : "Second player's turn: "
+    while(1)
+      move = gets.chomp.upcase.chars.sort
+      begin
+        if board[row.index(move[0])][col.index(move[1])] == nil # Valid move
+          board[row.index(move[0])][col.index(move[1])] = (turn == PLAYER_ONE ? "X" : "O")
+          return
+        end
+        print "Spot taken. Try again: "
+      rescue
+        print "Invalid move. Try again: "
       end
-   end
-   counter
-end
-
-def current_player(board)
-    turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def turn(board)
-   puts "Please enter 1-9:"
-   user_input = gets.strip
-   index = input_to_index(user_input)
-   if valid_move?(board, index)
-      move(board, index, current_player(board))
-      turn(board)
-   end
-   display_board(board)
-end
-
-def won?(board)
-   #Might need to redo this to meet own specs
-   WIN_COMBINATIONS.each do |win_combination|
-      # Comparisons between the three integer
-      # Use current_player for it to make it easier
-      # If true, break and go to finish
-   end
-end
-
-def full?(board)
-    board.all? {|i| i == "X" || i == "O"}
-end
-
-def draw?(board)
-    if !won?(board) && full?(board)
-      return true
-    elsif !won?(board) && !full?(board)
-      return false
-    else won?(board)
-      return false
     end
-end
+  end
 
-def over?(board)
-    if draw?(board) || won?(board) || full?(board)
-      return true
+  def draw_board(board, row, col)
+    puts "\n  " + col.join(" ")
+    row.each_with_index do |key, index|
+      puts key + " " + board[index].map{ |val| make_blanks(val, index) }.join("|")
     end
-end
+    puts "\n"
+  end
 
-def winner(board)
-    if won?(board)
-       return board[won?(board)[0]]
+  def make_blanks(val, index)
+    return " " if val == nil && index == 2
+    return "_" if val == nil
+    return val
+  end
+
+  def game_won?(board, turn)
+    row_win = board
+    col_win = board.transpose
+    dia_win = [[board[0][0],board[1][1],board[2][2]],[board[0][2],board[1][1],board[2][0]]]
+    wins = row_win + col_win + dia_win
+
+    wins.each { |try| return true if (try.uniq.length == 1 && try[0] != nil) }
+    return false
+  end
+
+  def play()
+    row_index = ["A","B","C"]
+    col_index = ["D","E","F"]
+    board = Array.new(3){ Array.new(3) }
+    turn = PLAYER_ONE
+
+    puts "\nLet's Play Tic-Tac-Toe\n"
+    draw_board(board, row_index, col_index)
+
+    9.times do
+      make_move(board, row_index, col_index, turn)
+      draw_board(board, row_index, col_index)
+      if(game_won?(board, turn))
+        puts "Congratulations. Player " + (turn == PLAYER_ONE ? "one" : "two") + " won!"
+        return
+      end
+      turn = (turn + 1) % 2
     end
+
+    puts "It's a tie!"
+  end
 end
 
-def play(board)
-    until over?(board) 
-       turn(board)
-    end 
-    if won?(board)
-       winner(board) == "X" || winner(board) == "O" 
-       puts "Congratulations #{winner(board)}!" 
-    elsif draw?(board)
-       puts "Cats Game!" 
-       end 
-end
-
-display_board(board)
-play(board)
-
-
-
-
-
+Game.new.play
